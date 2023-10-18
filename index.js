e7f04e7f268a94ba574b4e6a3b90ef78ef0ce5a1
Kaushik Sharma <kaushik445566@gmail.com>
2023-10-18 15:13:05 +0530

    js done

--- index.js
+++ index.js
@@ -499,3 +499,407 @@ document.addEventListener("keydown", (event) => {
     search();
   });
 });
+
+function sortTask() {
+  let sortType = document.getElementById("sort-task").value;
+  let tasks = getTasks();
+
+  tasks.sort(function (a, b) {
+    switch (sortType) {
+      case "date":
+        let temp = new Date(a.due) - new Date(b.due);
+        // console.log(temp);
+        return temp;
+      case "completed":
+        return b.status - a.status;
+      case "incompleted":
+        return a.status - b.status;
+      default:
+        return 0;
+    }
+  });
+  localStorage.setItem(`${currentLoginUser}-tasks`, JSON.stringify(tasks));
+
+  loadToUi(tasks);
+}
+
+function search() {
+  //   console.log("running");
+  let searchBarValue = document.getElementById("search-bar").value;
+  let filterType = document.getElementById("search-bar-filter").value;
+  let sortType = document.getElementById("sort-task").value;
+  //   console.log(filterType);
+  let tasks = getTasks();
+  console.log(searchBarValue);
+
+  searchedtasks = tasks.filter((task) => {
+    return task.title.toLowerCase().includes(searchBarValue.toLowerCase());
+  });
+
+  console.log(searchedtasks);
+  searchedtasks.sort(function (a, b) {
+    switch (sortType) {
+      case "date":
+        let temp = new Date(a.due) - new Date(b.due);
+        // console.log(temp);
+        return temp;
+      case "completed":
+        return b.status - a.status;
+      case "incompleted":
+        return a.status - b.status;
+      default:
+        return 0;
+    }
+  });
+
+  loadToUi(searchedtasks, filterType);
+}
+
+function filtertasks(val) {
+  document.getElementById("search-bar-filter").value = val;
+  document.getElementById("search-bar-filter").click();
+}
+
+function loadToUi(parsedData, type = "all") {
+  console.log(parsedData);
+  loadDateOrNav();
+  if (window.innerWidth <= 1024) {
+    document.getElementById("sidebar").classList.remove("left");
+  }
+  listEle.innerHTML = "";
+  let state = 0;
+
+  //   let data = localStorage.getItem("tasks");
+  //   parsedData = JSON.parse(data) || [];
+
+  // const workTaskList = document.createElement("div");
+  // workTaskList.classList.add("task-list-category-container");
+  // const personalTaskList = document.createElement("div");
+  // personalTaskList.classList.add("task-list-category-container");
+  // const studyTaskList = document.createElement("div");
+  // studyTaskList.classList.add("task-list-category-container");
+
+  if (parsedData.length > 0) {
+    parsedData.forEach((task, index) => {
+      let taskContainer = document.createElement("div");
+      taskContainer.classList.add("task-container");
+
+      let titleDropCon = document.createElement("div");
+      titleDropCon.classList.add("title-drop-con");
+      let title = document.createElement("div");
+      title.classList.add("title");
+      // title.innerHTML='<i class="fa-solid fa-candy-cane candy"></i>'
+      title.innerHTML = '<i class="fa-solid fa-thumbtack candy"></i>';
+      title.appendChild(document.createTextNode(task.title));
+
+      let menu = document.createElement("div");
+      menu.classList.add("menu");
+
+      let dots = document.createElement("div");
+      dots.classList.add("dots");
+
+      function createdot() {
+        let dot = document.createElement("div");
+        dot.classList.add("dot");
+        return dot;
+      }
+
+      let dropdown = document.createElement("div");
+      dropdown.classList.add("dropdown");
+
+      dots.appendChild(createdot());
+      dots.appendChild(createdot());
+      dots.appendChild(createdot());
+
+      // <div class="menu">
+      //     <div class="dots">
+      //         <div class="dot"></div>
+      //         <div class="dot"></div>
+      //         <div class="dot"></div>
+      //     </div>
+      //     <div class="dropdown">
+      //         <a href="#">Item 1</a>
+      //         <a href="#">Item 2</a>
+      //         <a href="#">Item 3</a>
+      //     </div>
+      // </div>
+
+      let deleteButton = document.createElement("button");
+      deleteButton.appendChild(document.createTextNode("delete task"));
+      deleteButton.classList.add("delete-button");
+      deleteButton.classList.add("drop-button");
+      deleteButton.onclick = () => deleteTask(task.id);
+
+      let statusButton = document.createElement("button");
+      statusButton.classList.add("status-button");
+      statusButton.classList.add("drop-button");
+      if (task.status == true) {
+        statusButton.appendChild(document.createTextNode("completed"));
+      } else {
+        statusButton.appendChild(document.createTextNode("incompleted"));
+      }
+      statusButton.onclick = () => {
+        //   console.log("clicked");
+        changeStatus(task.id);
+        sortTask();
+      };
+
+      dropdown.appendChild(statusButton);
+      dropdown.appendChild(deleteButton);
+
+      menu.appendChild(dots);
+      menu.appendChild(dropdown);
+
+      let description = document.createElement("div");
+      description.classList.add("desc");
+      let desc = task.desc;
+      if (task.desc.length > 60) {
+        desc = task.desc.substring(0, 60);
+        desc = desc.substring(0, desc.lastIndexOf(" ")) || desc.substring(0, 30);
+        desc = desc + "...";
+      }
+      description.appendChild(document.createTextNode(desc));
+
+      let categories = document.createElement("div");
+      categories.classList.add("category-con");
+
+      function createCategory(name) {
+        let category = document.createElement("div");
+        category.classList.add("category");
+        category.appendChild(document.createTextNode(name));
+        if (name == "work") {
+          category.style.background = "#BBEBDA";
+        } else if (name == "personal") {
+          category.style.background = "#CACEFF";
+        } else if (name == "study") {
+          category.style.background = "#FCE3AD";
+        }
+        return category;
+      }
+      console.log(task.category);
+      let categoriesArray = task.category;
+
+      if (categoriesArray.includes("work")) {
+        categories.appendChild(createCategory("work"));
+      }
+      if (categoriesArray.includes("personal")) {
+        categories.appendChild(createCategory("personal"));
+      }
+      if (categoriesArray.includes("study")) {
+        categories.appendChild(createCategory("study"));
+      }
+
+      const due = new Date(task.due);
+      let day = "Monday";
+      switch (due.getDay()) {
+        case 0:
+          day = "Sunday";
+          break;
+        case 1:
+          day = "Monday";
+          break;
+        case 2:
+          day = "Tuesday";
+          break;
+        case 3:
+          day = "Wednesday";
+          break;
+        case 4:
+          day = "Thursday";
+          break;
+        case 5:
+          day = "Friday";
+          break;
+        case 6:
+          day = "Saturday";
+      }
+
+      const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      let currentMonth = months[due.getMonth()];
+
+      let dateCon = document.createElement("div");
+      dateCon.classList.add("date-con");
+      let timericon = document.createElement("div");
+      timericon.innerHTML = '<i class="fa-solid fa-clock-rotate-left timer"></i>';
+      timericon.classList.add("timer-icon");
+
+      let date = document.createElement("div");
+      date.classList.add("due-date");
+      date.appendChild(
+        document.createTextNode(`${due.getDate()} ${currentMonth} ${due.getFullYear()}`),
+      );
+
+      dateCon.appendChild(timericon);
+      dateCon.appendChild(date);
+
+      // console.log(day,currentMonth,due.getDate());
+      let userCon = document.createElement("div");
+      userCon.classList.add("user-con");
+      let usrName = document.createElement("div");
+      usrName.classList.add("login-user-name");
+      usrName.appendChild(document.createTextNode(currentLoginUser));
+
+      let userWrapper = document.createElement("div");
+      userWrapper.classList.add("user-wrapper");
+      let userAvatarNameWrapper = document.createElement("div");
+      userAvatarNameWrapper.classList.add("userAvatarNameWrapper");
+      userAvatarNameWrapper.innerHTML = '<img src="./img/avatar.png" class="avatar" />';
+      userAvatarNameWrapper.appendChild(usrName);
+      userWrapper.appendChild(userAvatarNameWrapper);
+      let stateContainer = document.createElement("div");
+      stateContainer.style.cursor = "pointer";
+      stateContainer.onclick = () => {
+        console.log(task.id);
+        changeStatus(task.id);
+      };
+      stateContainer.classList.add("state");
+      if (task.status) {
+        stateContainer.innerHTML = '<i class="fa-regular fa-circle-check state-icon"></i>';
+        stateContainer.appendChild(document.createTextNode("Done"));
+        stateContainer.style.background = "#94FFCA";
+      } else {
+        stateContainer.innerHTML = '<i class="fa-regular fa-circle-xmark state-icon"></i>';
+        stateContainer.appendChild(document.createTextNode("Pending"));
+        stateContainer.style.background = "#FED7D1";
+      }
+      userWrapper.appendChild(stateContainer);
+      userCon.appendChild(userWrapper);
+      title.onclick = () => {
+        console.log("click working");
+        editTaskPanel.style.right = "0%";
+        let editTitle = document.querySelector(".edit-title");
+        let editdesc = document.querySelector(".edit-desc");
+        let editdate = document.querySelector(".edit-date");
+        editTitle.innerHTML = task.title;
+        editdesc.innerHTML = task.desc;
+        editdate.innerHTML = `${due.getDate()} ${currentMonth} ${due.getFullYear()}`;
+        editwork.classList.remove("highlight-cat");
+        editpersonal.classList.remove("highlight-cat");
+        editstudy.classList.remove("highlight-cat");
+        work.classList.remove("highlight-cat");
+        personal.classList.remove("highlight-cat");
+        study.classList.remove("highlight-cat");
+        savecategoryarray();
+        savecategoryarray(task.category);
+        task.category.forEach((item) => {
+          switch (item) {
+            case "work":
+              editwork.classList.add("highlight-cat");
+              break;
+            case "personal":
+              editpersonal.classList.add("highlight-cat");
+              break;
+            case "study":
+              editstudy.classList.add("highlight-cat");
+              break;
+          }
+        });
+        let save = document.querySelector(".save");
+        save.onclick = () => {
+          let alltasks = getTasks();
+          console.log(task);
+          alltasks.map((item) => {
+            if (item.id == task.id) {
+              item.title = editTitle.innerHTML;
+              item.desc = editdesc.innerHTML;
+              item.category = getcategoryarray();
+            }
+          });
+
+          localStorage.setItem(`${currentLoginUser}-tasks`, JSON.stringify(alltasks));
+          search();
+          editTaskPanel.style.right = "-100%";
+        };
+      };
+
+      titleDropCon.appendChild(title);
+      titleDropCon.appendChild(menu);
+
+      let deletebutton2 = document.createElement("div");
+      deletebutton2.classList.add("delete-icon-button");
+      deletebutton2.innerHTML = '<i class="fa-solid fa-trash delete-icon-button"></i>';
+      deletebutton2.onclick = () => deleteTask(task.id);
+      titleDropCon.appendChild(deletebutton2);
+      taskContainer.appendChild(titleDropCon);
+      taskContainer.appendChild(description);
+      taskContainer.appendChild(categories);
+      taskContainer.appendChild(dateCon);
+      taskContainer.appendChild(userCon);
+      function addOrNot(list, taskcon, task) {
+        //   console.log(type);
+        if (type == "all") {
+          state++;
+          list.appendChild(taskcon);
+        } else if (type == "incompleted") {
+          if (task.status == false) {
+            state++;
+            list.appendChild(taskcon);
+          }
+        } else if (type == "completed") {
+          if (task.status == true) {
+            state++;
+            list.appendChild(taskcon);
+          }
+        }
+      }
+
+      // switch (task.category) {
+      //   case "work":
+      //     addOrNot(workTaskList, taskContainer, task);
+      //     break;
+      //   case "personal":
+      //     addOrNot(personalTaskList, taskContainer, task);
+      //     break;
+      //   case "study":
+      //     addOrNot(studyTaskList, taskContainer, task);
+      //     break;
+      // }
+
+      addOrNot(listEle, taskContainer, task);
+      console.log(state);
+
+      // listEle.appendChild(taskContainer)
+    });
+  }
+
+  if (state == 0) {
+    console.log("no data");
+    listEle.innerHTML = `<img src="./img/no.png" class="no-data" />`;
+  }
+
+  // listEle.appendChild(workTaskList);
+  // listEle.appendChild(personalTaskList);
+  // listEle.appendChild(studyTaskList);
+}
+
+function getTasks() {
+  return JSON.parse(localStorage.getItem(`${currentLoginUser}-tasks`)) || [];
+}
+function saveTasks() {
+  return JSON.parse(localStorage.getItem(`${currentLoginUser}-tasks`)) || [];
+}
+function creatcategoryarray() {
+  localStorage.setItem("categoriesArray", JSON.stringify([]));
+}
+function getcategoryarray() {
+  if (!JSON.parse(localStorage.getItem("categoriesArray"))) {
+    localStorage.setItem("categoriesArray", JSON.stringify([]));
+  }
+  return JSON.parse(localStorage.getItem("categoriesArray")) || [];
+}
+function savecategoryarray(data = []) {
+  localStorage.setItem("categoriesArray", JSON.stringify(data));
+}
